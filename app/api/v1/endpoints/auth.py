9e6ae552d1074d3f18555c635f16bdb3bbeba543
bazed-lab <gghg554565@gmail.com>
2026-06-08 23:41:20 +0300

    Add in auth.py two endpoints singup and login

--- app/api/v1/endpoints/auth.py
+++ app/api/v1/endpoints/auth.py
@@ -1,11 +1,10 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from core.security import hash_password, verify_password, create_access_token, create_refresh_token
 from db.session import get_db
-from models.user import User
-from repositories.repositories import UserRepository
-from schemas.auth import UserRegisterRequest, UserLoginRequest, TokenResponse, UserResponse
+from schemas.auth import UserRegisterRequest, UserLoginRequest, TokenResponse
+from services.auth_service import AuthService
+from core.security import create_access_token, create_refresh_token
 
 auth_router = APIRouter()
 
@@ -15,11 +14,17 @@ async def signup(
         payload: UserRegisterRequest,
         session: AsyncSession = Depends(get_db)
         ):
-    pass
+    service = AuthService(session)
+    user = await service.singup(email=payload.email, password=payload.password, username=payload.username)
+    access_token, _ = create_access_token(user.id)
+    refresh_token, _ = create_refresh_token(user.id)
+    return TokenResponse(access_token=access_token, refresh_token=refresh_token, token_type="bearer")
 
 
 @auth_router.post("/login", response_model=TokenResponse)
 async def login(payload: UserLoginRequest,
                 session: AsyncSession = Depends(get_db)
                 ):
-    pass
+    service = AuthService(session)
+    user, access_token, refresh_token, token_type = await service.login(email=payload.email, password=payload.password)
+    return TokenResponse(access_token=access_token, refresh_token=refresh_token, token_type=token_type)
